c1594768ee6585ed79460f4905001e809d1957c4
Dustin Dortch <me@dustindortch.com>
2019-07-05 13:56:17 -0400

    Updated PowerShell on the River presentation

---
+++ PowerShell Identity Automation with MIM/00 - VS Code Setup.ps1
@@ -0,0 +1,5 @@
+# Change Theme: <Ctrl> + <Shift> + P, Preferences: *Color Theme*, PowerShell ISE
+# Screenccast Mode: <Ctrl> + <Shift> + P, Developer: *Screencast Mode*
+# Toggle Terminal: <Ctrl> + J
+# Toggle Folders: <Ctrl> + B
+# Zoom In: <Ctrl> + <Scrollwheel>
